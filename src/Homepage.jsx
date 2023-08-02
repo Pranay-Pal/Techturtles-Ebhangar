7bb4b4fc84ad231e962a795bacf245dc5ae37a8e
Sourav <sn913929@gmail.com>
2023-08-02 22:50:55 +0530

    2nd commit

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,6 +1,32 @@
 import "./Homepage.css"
+import { Icon } from '@iconify/react';
+import Logo from "./assets/logo.png"
 export default function Homepage() {
-    return (<><h1 className="hat">Hand</h1>
-        <h1 className="hat">And</h1>
+    return (
+        <>
+        <div className="header">
+            <Icon className="icon" icon="ic:baseline-menu"/>
+            <a className="Logo">
+                <img src={Logo} alt="LOGO" className="LogoImg"/>
+            </a>
+            <button className="button-17" role="button">Signin</button>
+        </div>
+        <main>
+            <div className="container">
+                <div className="cardItems">
+                    <div><h3>sell</h3></div>
+                </div>
+                <div className="cardItems">
+                    <div><h3>sell</h3></div>
+                </div>
+                <div className="cardItems">
+                    <div><h3>sell</h3></div>
+                </div>
+                <div className="cardItems">
+                    <div><h3>sell</h3></div>
+                </div>
+            </div>
+        </main>
+        <footer></footer>
         </>)
 }
